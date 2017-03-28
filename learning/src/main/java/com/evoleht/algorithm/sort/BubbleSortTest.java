619011ddb17e6f661ee27c4cb98e5b71056aadc3
evoleht <evoleht@163.com>
2017-03-28 17:57:06 +0800

    readme

---
+++ learning/src/main/java/com/evoleht/algorithm/sort/BubbleSortTest.java
@@ -0,0 +1,41 @@
+package com.evoleht.algorithm.sort;
+
+/**
+ * 冒泡排序
+ * 原理：
+ * 设数组长度为N
+ * 1.对数组相邻的两个元素进行比较，如果j > j+1 则交换。
+ * 2.对数组第0个数据到N-1个数据的一次遍历可以将最大的一个数沉到第N-1的位置
+ * 2.N=N-1，如果N！=0 则继续循环
+ *  
+ * @author wangzs
+ * @version v1.0.0
+ * @since v1.0.0
+ */
+public class BubbleSortTest {
+	
+	public int[] sort(int arr[]) {
+		int[] result_arr = arr;
+		int len = result_arr.length;
+		for (int i = 0; i < result_arr.length; i++) {
+			for (int j = 0; j < len-1; j++) {
+				if (result_arr[j] < result_arr[j+1]) {
+					int tmp = result_arr[j];
+					result_arr[j] = result_arr[j+1];
+					result_arr[j+1] = tmp;
+				}
+			}
+			len--;
+		}
+		return result_arr;
+	}
+	
+	public static void main(String[] args) {
+		int arr[] = {3,6,2,8,1,20,32,11};
+		BubbleSortTest sortTest = new BubbleSortTest();
+		int [] resunt = sortTest.sort(arr);
+		for (int i = 0; i < resunt.length; i++) {
+			System.out.print(resunt[i]+" ");
+		}
+	}
+}
