0978fc9a364d2a471a7e49cbbc0bda05710b6c81
evoleht <evoleht@163.com>
2017-04-01 18:19:30 +0800

    java io

---
+++ learning/src/main/java/com/evoleht/DesignPatterns/decorator/javaIO/Test.java
@@ -0,0 +1,13 @@
+package com.evoleht.DesignPatterns.decorator.javaIO;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Test {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		InputStream io = new FileInputStream("");
+	}
+}
