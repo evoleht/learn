e74c1f74454adb63cf590c6c4d78929f924b4593
evoleht <evoleht@163.com>
2017-04-07 14:22:27 +0800

    thread interview

---
+++ learning/src/main/java/com/evoleht/thread/interview/ThreadTest.java
@@ -0,0 +1,49 @@
+package com.evoleht.thread.interview;
+
+public class ThreadTest {
+	
+	public void methodA() {
+		int i = 3;
+		synchronized (this) {
+			while (i-- > 0) {
+				System.out.println(i);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public void methodB() {
+		int i = 3;
+		while (i-- > 0) {
+			System.out.println(i);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public static void main(String[] args) {
+		final ThreadTest threadTest = new ThreadTest();
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				threadTest.methodA();
+			}
+		});
+		
+		Thread t2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				threadTest.methodB();
+			}
+		});
+		
+		t1.start();
+		t2.start();
+	}
+}
