0023017f3aacf11bcc8bcba741669a332fabb619
evoleht <evoleht@163.com>
2017-04-13 17:54:14 +0800

    executor

---
+++ learning/src/main/java/com/evoleht/base/baseclass/collection/list/LinkedListTest.java
@@ -0,0 +1,9 @@
+package com.evoleht.base.baseclass.collection.list;
+
+import java.util.LinkedList;
+
+public class LinkedListTest {
+	public static void main(String[] args) {
+		LinkedList<String> linkedList = new LinkedList<String>();
+	}
+}
