723d55d767585ffef391e0b8234ba1bf6a056a64
evoleht <evoleht@163.com>
2017-05-07 00:20:58 +0800

    1

---
+++ learning/src/main/java/com/evoleht/base/baseclass/concurrents/blockingqueue/delayqueuetest/DelayQueueTest.java
@@ -0,0 +1,8 @@
+package com.evoleht.base.baseclass.concurrents.blockingqueue.delayqueuetest;
+
+import java.util.concurrent.DelayQueue;
+
+public class DelayQueueTest {
+	
+	DelayQueue dqueue = new DelayQueue();
+}
